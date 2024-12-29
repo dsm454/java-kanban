91423f597f73366d140e4d04d479ebf4050b0a5a
Dmitrii Kuznetsov <dsm454@yandex.ru>
2024-12-29 17:54:27 +0500

    the first working version

---
+++ src/tasks/Task.java
@@ -0,0 +1,58 @@
+package tasks;
+
+import java.util.Objects;
+
+public class Task {
+    protected String name;        // Название, описывающее задачу (например, «Переезд»).
+    protected String description; // Описание
+    protected int id;             // id задачи, по которому её можно будет найти.
+    protected Status status;
+
+    public Task(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.status = Status.NEW;
+    }
+    public Task(String name, String description, int id) {
+        this(name, description);
+        this.id = id;
+    }
+
+    public Task(String name, String description, int id, Status status) {
+        this(name, description, id);
+        this.status = status;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public Status getStatus() { return status; }
+    public void setStatus(Status status) { this.status = status; }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o ) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return id == task.id && Objects.equals(name, task.name)
+                  && Objects.equals(description, task.description)
+                  && status == task.status;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
+
+}
